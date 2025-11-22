c8f3227c8046e69c6e452d4df5923f73033462b7
Suhan2222 <niggehehe23@gmail.com>
2025-11-22 19:19:54 +0545

    efef

--- javarikshawfare.java
+++ javarikshawfare.java
@@ -6,7 +6,7 @@ import java.util.*;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class RickshawFareCalculator {
+public class javarikshawfare {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
