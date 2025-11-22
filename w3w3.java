8e47b3e3718610a53d548801074a51e34bd4b6ba
Suhan2222 <niggehehe23@gmail.com>
2025-11-22 19:14:07 +0545

    Initial sharing of project

---
+++ w3w3.java
@@ -0,0 +1,17 @@
+/**
+ * Write a description of class w3w3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class w3w3
+{
+    public static void main (String [] args){
+    int a=10;
+    int b=10;
+    System.out.println(++a);
+    System.out.println(a++);
+
+}
+
+}
